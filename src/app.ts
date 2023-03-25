a845616c80ef63329cdee532dbbae5858676ab8e
Son Dang <minhson0506@gmail.com>
2023-03-25 13:50:34 +0200

    accept origin all

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ import api from './api'
 const app = express()
 app.use(morgan('dev'))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
+
 app.use(express.json())
 
 app.get<{}, MessageResponse>('/', (req, res) => {
